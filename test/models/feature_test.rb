0f03354bd97d9744dcc4ef207114b364e24b1829
Reinier Rodriguez Gonzalez <reinier.rodriguez@busolinea.com>
2020-01-24 00:23:43 -0600

    Added tests for Image and Feature Models

--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class FeatureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create' do
+    feature = Feature.create name: 'Roof Garden'
+    assert feature.save
+
+    feature2 = Feature.create name: 'Roof Garden'
+    refute feature2.save
+  end
+
+  test 'required fields' do
+    feature = Feature.create
+    refute feature.valid?
+
+    feature.name = 'Patio'
+    assert feature.valid?
+  end
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'create' do
+    img = Image.create url: 'http://images.com/image1', order: 54
+    assert img.save
+
+    img2 = Image.create url: 'http://images.com/image1', order: 54
+    refute img2.save
+
+    img2.url = 'http://images.com/image2'
+    refute img2.save
+
+    img2.url = 'http://images.com/image1'
+    img2.order = 55
+    refute img2.save
+
+    img2.url = 'http://images.com/image2'
+    assert img2.save
+  end
+
+  test 'required fields' do
+    img = Image.create
+    refute img.valid?
+
+    img.url = 'http://images.com/image1'
+    refute img.valid?
+
+    img.url = nil
+    img.order = 55
+    refute img.valid?
+
+    img.url = 'http://images.com/image1'
+    assert img.valid?
+  end
 end
