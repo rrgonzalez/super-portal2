a53a724b2edde703fb99f432d649c6387b2d3645
Reinier Rodriguez Gonzalez <reinier.rodriguez@busolinea.com>
2020-01-24 00:50:34 -0600

    Updated property_test.rb to check for new fields

--- test/models/property_test.rb
+++ test/models/property_test.rb
@@ -9,7 +9,50 @@ class PropertyTest < ActiveSupport::TestCase
     property.sale = true
     property.currency = currencies(:usd)
     property.user = users(:bart)
+    property.external_id = '384s'
+    property.neighborhood = 'Roma Norte'
     assert property.save
+
+    property2 = Property.new
+    property2.title = 'Another Cool House'
+    property2.description = 'Super description of cool house'
+    property2.property_type = property_types(:house)
+    property2.sale = true
+    property2.currency = currencies(:usd)
+    property2.user = users(:bart)
+    property2.external_id = '384s'
+    property2.neighborhood = 'Vertiz Narvarte'
+    refute property2.save
+
+    property2.external_id = '235r'
+    assert property2.save
+  end
+
+  test 'required fields' do
+    property = Property.new
+    refute property.valid?
+
+    property.title = 'Cool House'
+    refute property.valid?
+
+    property.description = 'Super description of cool house'
+    refute property.valid?
+
+    property.property_type = property_types(:house)
+    refute property.valid?
+
+    property.currency = currencies(:mxn)
+    refute property.valid?
+
+    property.user = users(:bart)
+    refute property.valid?
+
+    property.rental = true
+    property.external_id = '31203r'
+    refute property.valid?
+
+    property.neighborhood = 'Condesa'
+    assert property.valid?
   end
 
   test 'operation_present?' do
